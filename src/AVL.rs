85b65d793753113d728b92a41a252248cbb692db
MikeWang <297600793@qq.com>
2021-11-07 01:46:11 -0600

    Initial finish AVL.rs

--- src/AVL.rs
+++ src/AVL.rs
@@ -1,9 +1,9 @@
 use core::cmp::{max, Ordering};
 use core::mem::swap;
+
 use DeleteValue::*;
 use InnerResult::*;
-// Total 339 lines
-// smart pointer with Box<T>
+
 pub type AvlTreeNode<T> = Option<Box<TreeNode<T>>>;
 
 #[derive(Clone, Debug)]
@@ -15,16 +15,22 @@ pub struct TreeNode<T: PartialOrd> {
 }
 
 enum InnerResult {
-    Left,     //在左子树完成插入
-    Right,    //在右子树完成插入
-    Unknown,  //树的平衡性未知
+    Left,
+    //在左子树完成插入
+    Right,
+    //在右子树完成插入
+    Unknown,
+    //树的平衡性未知
     Balanced, //树已确定平衡
 }
 
 enum DeleteValue<T: PartialOrd> {
-    Min,                 //匹配最小节点
-    Max,                 //匹配最大节点
-    Val(T),              //匹配给定值
+    Min,
+    //匹配最小节点
+    Max,
+    //匹配最大节点
+    Val(T),
+    //匹配给定值
     Del(AvlTreeNode<T>), //返回被删除节点
 }
 
@@ -49,36 +55,51 @@ impl<T: PartialOrd> PartialOrd<Box<TreeNode<T>>> for DeleteValue<T> {
         }
     }
 }
-// 定义私有方法，仅限于内部操作 - internal ONLY
+
 trait __AvlTree<T: PartialOrd> {
-    fn left_rotate(&mut self);  // Basic ll - 左旋
-    fn right_rotate(&mut self);  // Basic rr - 右旋转
+    fn right_rotate(&mut self);
+    // 右旋转 - ll
+    fn left_rotate(&mut self);
+    // 左旋转 - rr
     fn rotate_lr(&mut self);
     fn rotate_rl(&mut self);
-    fn update_height(&mut self);  // 更新节点高度
-    fn balance_factor(&self) -> i32;  // 更新平衡因子
-    fn do_insert(&mut self, val: T) -> InnerResult;  // 添加
-    fn do_delete(&mut self, val: &mut DeleteValue<T>) -> InnerResult;  // 删除
-}
-// 定义公有方法，用户接口 - user interface
-pub trait AvlTree<T: PartialOrd> {
-    fn new(val: T) -> Self;  // 新建节点
-    fn height(&self) -> i32; // 获取高度
-    fn insert(&mut self, val: T);  // 添加节点
-    fn delete(&mut self, val: T) -> Self;  // 删除节点
+    fn update_height(&mut self);
+    fn balance_factor(&self) -> i32;
+    fn do_insert(&mut self, val: T) -> InnerResult;
+    fn do_delete(&mut self, val: &mut DeleteValue<T>) -> InnerResult;
 }
 
-// 编写私有方法 - 内部操作使用，用户不可见！
+pub trait AvlTree<T: PartialOrd> {
+    fn new(val: T) -> Self;
+    fn height(&self) -> i32;
+    fn insert(&mut self, val: T);
+    fn delete(&mut self, val: T) -> Self;
+}
+
 impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
-    fn left_rotate(&mut self) {
+    //         y                            x
+    //        / \     Right Rotation       / \
+    //       x  T4    ==============>     z   y
+    //      / \                          /\ / \
+    //     z  T3                        1 2 3  4
+    //   T1 T2
+    fn right_rotate(&mut self) {
         match self {
-            Some(root) => {
+            Some(root) => {  // y is root
+                // 1. 拿到root的左侧子树，即x分支(此时左侧子树已经剥离)
                 let left = &mut root.left.take();
                 match left {
-                    Some(x) => {
-                        swap(&mut root.left, &mut x.right);
-                        self.update_height();
-                        swap(self, &mut x.right);
+                    // 如果左侧子树(x-branch)有东西
+                    Some(node) => {
+                        // 2. T3连接至y的左侧(root的左侧和x的右侧互换)
+                        // root.left=x.right & x.right=root.left
+                        swap(&mut root.left, &mut node.right);
+                        self.update_height();  // 更新高度
+                        // 此时self是y-(T3 & T4)
+                        // 3. 将y连接至x分支的右侧(此时root变为了x)
+                        swap(self, &mut node.right);
+                        // 4. 将重新整合好的x分支(left变量)赋给self
+                        // 此时self是整合好之后的x分支
                         swap(self, left);
                         self.update_height();
                     }
@@ -90,24 +111,27 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
     }
 
     //         y                            x
-    //        / \     Right Rotation       / \
-    //       x   T4    ==============>    z   y
-    //      / \                          / \ / \
-    //     z   T3                       1  2 3 4
-    //   T1 T2
-    // 对以一个节点为root的树进行右旋转
-    fn right_rotate(&mut self) {
+    //        / \     Left  Rotation       / \
+    //       T4  x    ==============>     y   z
+    //          / \                      / \ / \
+    //         T3  z                    4  3 2  1
+    //           T2 T1
+    fn left_rotate(&mut self) {
         match self {
-            Some(root) => {
-                // 1. 拆解 x 的右侧子树 T3
+            Some(root) => { // 此时root是y
+                // 1. 拿到y的右侧子树，即x分支(此时子树已经剥离)
                 let right = &mut root.right.take();
                 match right {
-                    // 如果有，那么
-                    Some(x) => {
-
-                        swap(&mut root.right, &mut x.left);
+                    // 如果x分支不为空
+                    Some(node) => {
+                        // 2. 将x的左侧和y的右侧交换(即 y-(T4 & T3))
+                        swap(&mut root.right, &mut node.left);
                         self.update_height();
-                        swap(self, &mut x.left);
+                        // 此时self是y-(T4 & T3)
+                        // 3.将x的左侧连接上self(即y分支)，此时root变为x
+                        swap(self, &mut node.left);
+                        // 4.将重新整合好的x分支赋值给right变量
+                        // 此时self是x分支
                         swap(self, right);
                         self.update_height();
                     }
@@ -121,8 +145,8 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
     fn rotate_lr(&mut self) {
         match self {
             Some(root) => {
-                root.left.right_rotate();
-                self.left_rotate();
+                root.left.left_rotate();
+                self.right_rotate();
             }
             None => unreachable!(),
         }
@@ -131,8 +155,8 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
     fn rotate_rl(&mut self) {
         match self {
             Some(root) => {
-                root.right.left_rotate();
-                self.right_rotate();
+                root.right.right_rotate();
+                self.left_rotate();
             }
             None => unreachable!(),
         }
@@ -141,20 +165,22 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
     fn update_height(&mut self) {
         match self {
             None => {}
-            Some(x) => x.height = max(x.left.height(), x.right.height()) + 1,
+            // 找到左侧子树和右侧子树中最高的高度，再加上本身的1，就是自己的高度
+            Some(node) => node.height = max(node.left.height(), node.right.height()) + 1,
         }
     }
 
     fn balance_factor(&self) -> i32 {
         match self {
             None => 0,
-            Some(x) => x.left.height() - x.right.height(),
+            // 平衡因子 = 左侧子树高度 - 右侧子树高度
+            Some(node) => node.left.height() - node.right.height(),
         }
     }
 
     fn do_insert(&mut self, val: T) -> InnerResult {
         match self {
-            //直接插入新节点
+            //如果某个指定位置没有节点，就新建一个放在这里
             None => {
                 *self = Self::new(val);
                 Unknown
@@ -164,25 +190,28 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
                 //重复数据
                 if val == root.val {
                     Balanced
-                    //进入左子树递归插入
                 } else if val < root.val {
+                    // 目标值 < 当前节点值，向左侧子树寻找位置
                     match root.left.do_insert(val) {
                         Balanced => Balanced,
-                        x => {
+                        NotBalanced => {
+                            // 当"平衡因子"绝对值大于1的时候就是不平衡，此时是正数，代表左侧不平衡
                             if self.balance_factor() == 2 {
-                                match x {
-                                    Left => self.left_rotate(),
-                                    Right => self.rotate_lr(),
-                                    _ => unreachable!(),
+                                match NotBalanced {
+                                    Left => self.right_rotate(), // Case: LeftLeft - ll
+                                    Right => self.rotate_lr(), // Case LeftRight - lr
+                                    _ => unreachable!(), //返回 `Unknown` 的时候当前节点必定是平衡的
                                 }
-                                Balanced
+                                Balanced  // 操作之后树已经平衡
                             } else if self.height() == {
+                                // 验证此时(after rotate)的高度是否和节点内部记录的一样
                                 self.update_height();
                                 self.height()
                             } {
+                                // 这里相当于 else if self.height = self.height {Balanced}
                                 Balanced
                             } else {
-                                Left
+                                Left  // 如果插入之后既平衡且高度一致，直接记录"左侧完成插入"
                             }
                         }
                     }
@@ -190,11 +219,12 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
                 } else {
                     match root.right.do_insert(val) {
                         Balanced => Balanced,
-                        x => {
+                        NotBalanced => {
+                            // 当"平衡因子"绝对值大于1的时候就是不平衡，此时是负数，代表右侧不平衡
                             if self.balance_factor() == -2 {
-                                match x {
-                                    Left => self.rotate_rl(),
-                                    Right => self.right_rotate(),
+                                match NotBalanced {
+                                    Left => self.rotate_rl(),  // case: RightLeft - rl
+                                    Right => self.left_rotate(),  // case: RightRight - rr
                                     _ => unreachable!(),
                                 }
                                 Balanced
@@ -215,14 +245,16 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
 
     fn do_delete(&mut self, val: &mut DeleteValue<T>) -> InnerResult {
         match self {
+            // 如果这个地方没有值，那就"什么都不做"
             None => {
-                *val = Del(None);
+                *val = Del(None); // 用delete(None)代表do nothing
                 Balanced
             }
+            // 如果有
             Some(root) => {
+                // 先搞到以这个节点为跟的树(或子树)的高度，存起来备用
                 let height = root.height;
-
-                //删除当前节点
+                // case 1:如果要找的就是当前这个
                 if val == root {
                     if root.left.is_some() {
                         //左右子树均非空
@@ -231,8 +263,8 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
                                 *val = Max;
                                 root.left.do_delete(val);
                                 match val {
-                                    Del(Some(x)) => {
-                                        swap(&mut root.val, &mut x.val);
+                                    Del(Some(node)) => {
+                                        swap(&mut root.val, &mut node.val);
                                     }
                                     _ => unreachable!(),
                                 }
@@ -269,7 +301,7 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
                                 if right.left.height() > right.right.height() {
                                     self.rotate_rl();
                                 } else {
-                                    self.right_rotate();
+                                    self.left_rotate();
                                 }
                             } else {
                                 self.update_height();
@@ -285,7 +317,7 @@ impl<T: PartialOrd> __AvlTree<T> for AvlTreeNode<T> {
                             if self.balance_factor() == 2 {
                                 let left = self.as_ref().unwrap().left.as_ref().unwrap();
                                 if left.left.height() >= left.right.height() {
-                                    self.left_rotate();
+                                    self.right_rotate();
                                 } else {
                                     self.rotate_lr();
                                 }
